10c05f760585c5549b964e110b5d6964bc369822
aambrozy <ambrozyaniko0@gmail.com>
2024-01-09 10:59:15 +0100

    teszt futtatatas miatti file modosulasok

--- Test Suites/TestSuite1.ts
+++ Test Suites/TestSuite1.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>b6b721f5-5314-4d25-b0c2-8790776f36ae</testSuiteGuid>
+   <testCaseLink>
+      <guid>329393f1-27df-49ad-bc4e-d4ad317c1064</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1 by Tom</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
